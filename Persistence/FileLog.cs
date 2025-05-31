b635e7c094725bb8541e5872c41d2f96fe3b2e55
lombichh <lombardi.matteo484@gmail.com>
2025-05-31 10:26:03 +0200

    add logfile

---
+++ Persistence/FileLog.cs
@@ -0,0 +1,11 @@
+using cashmonkey.Models;
+
+namespace cashmonkey.Persistence
+{
+    public static class FileLog
+    {
+
+        public static Log VociLog { get; } = new Log();
+        
+    }
+}
--- obj/Debug/net9.0/cashmonkey.AssemblyInfo.cs
+++ obj/Debug/net9.0/cashmonkey.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cashmonkey")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3f80a979ee717c6ea7035d7bfe767cb6e9cf047")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ba3dd09760d5ab7cb75b9d3b89e3546a8e3cb97")]
 [assembly: System.Reflection.AssemblyProductAttribute("cashmonkey")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cashmonkey")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
